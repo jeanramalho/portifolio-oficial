8adaa2dcb8f4de0926215806f208c3d6b27d8334
jeanramalho.dev <jeanramalho.dev@gmail.com>
2024-11-13 23:18:03 -0300

    Adiciona feature de digitação

--- index.js
+++ index.js
@@ -4,8 +4,38 @@ tailwind.config = {
       extend: {
         colors: {
             doura: '#b09c5e',
-        }
+        },
       }
     }
   }
+
+const words = ["Web Developer", "Swift Developer", "React Native Developer"];
+let i = 0;
+let j = 0;
+let currentWord = "";
+let isDeleting = false;
+
+function type() {
+  currentWord = words[i];
+  if (isDeleting) {
+    document.getElementById("typewriter").textContent = currentWord.substring(0, j-1);
+    j--;
+    if (j == 0) {
+      isDeleting = false;
+      i++;
+      if (i == words.length) {
+        i = 0;
+      }
+    }
+  } else {
+    document.getElementById("typewriter").textContent = currentWord.substring(0, j+1);
+    j++;
+    if (j == currentWord.length) {
+      isDeleting = true;
+    }
+  }
+  setTimeout(type, 185);
+}
+
+type();
 
