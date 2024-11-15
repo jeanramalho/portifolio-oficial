38a314a57958fb4cafd110e750360d0c6195a475
jeanramalho.dev <jeanramalho.dev@gmail.com>
2024-11-15 15:06:14 -0300

    Inicia Projeto

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
 tailwind.config = {
-    theme: {
+  darkMode: 'class',
+  theme: {
       extend: {
         colors: {
             doura: '#b09c5e',
@@ -9,33 +10,5 @@ tailwind.config = {
     }
   }
 
-const words = ["Web Developer", "Swift Developer", "React Native Developer"];
-let i = 0;
-let j = 0;
-let currentWord = "";
-let isDeleting = false;
 
-function type() {
-  currentWord = words[i];
-  if (isDeleting) {
-    document.getElementById("typewriter").textContent = currentWord.substring(0, j-1);
-    j--;
-    if (j == 0) {
-      isDeleting = false;
-      i++;
-      if (i == words.length) {
-        i = 0;
-      }
-    }
-  } else {
-    document.getElementById("typewriter").textContent = currentWord.substring(0, j+1);
-    j++;
-    if (j == currentWord.length) {
-      isDeleting = true;
-    }
-  }
-  setTimeout(type, 185);
-}
-
-type();
 
